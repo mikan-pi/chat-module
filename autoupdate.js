e1f59eb7a204163ed61e6311330d1725735e76ca
4q0c <yujin40401@yahoo.co.jp>
2025-04-03 18:38:50 +0900

    不要なコマンドの削除

--- autoupdate.js
+++ autoupdate.js
@@ -3,22 +3,22 @@
 // ファイル写し。
 import "./index"
 
-let old = ""
-let Imnew = ""
-
 let rege = /^mikan-pi-chat-module-\w+$/
 
-register("command", () => {
-    old = item.sha
+register("gameLoad", () => {
+    test = JSON.parse(readfile("ChatMi", "data/data.json"))
 
-    hash("mikan-pi", "chat-module", "main", (sha) => {
-        if (sha) {  // sha をチェックする
-            Update.start()
-        } else {
-            ChatLib.chat("失敗");
-        }
-    })
-}).setName("mi-test-demo");
+        hash("mikan-pi", "chat-module", "main", (sha) => {
+            if (test.sha !== sha) {  // sha をチェックする
+                Update.start()
+            }
+        }) 
+})
+
+function deletefile() {
+    FileLib.deleteDirectory(`${Config.modulesFolder}/${whatfileName()}`)
+    FileLib.deleteDirectory(`${Config.modulesFolder}/ChatMi.zip`)
+}
 
 const File = Java.type("java.io.File")
 const URL = Java.type("java.net.URL")
@@ -45,12 +45,6 @@ function urlToFile(url, destination, connecttimeout, readtimeout) {
 
 const Update = new Thread(() => {
     try {
-
-        if (old !== Imnew) {
-
-
-        } 
-
         urlToFile("https://api.github.com/repos/mikan-pi/chat-module/zipball", `${Config.modulesFolder}/ChatMi.zip`, 1000, 2000)
         ChatLib.chat(`download zip file! 1/5`)
         Thread.sleep(1000)
@@ -60,39 +54,12 @@ const Update = new Thread(() => {
         Thread.sleep(1000)
         ChatLib.chat(`unzip file! 2/5`)
         Thread.sleep(1000)
-        return
-        
-        if (old !== Imnew) {
-            /*
-            FileLib.deleteDirectory(`${Config.modulesFolder}/ChatMi`)
-            ChatLib.chat(`delete file! 3/5`)
-            Thread.sleep(1000)
-            */
-        } else {
-            FileLib.deleteDirectory(`${Config.modulesFolder}/mikan-pi-chat-module-4960d27`)
-            FileLib.deleteDirectory(`${Config.modulesFolder}/ChatMi.zip`)
-            ChatLib.chat(`現在のChatMiのバージョンは最新版です。`)
-            Thread.sleep(1000)
-            return
-        }
 
-        const unzippedDir = new File(`${Config.modulesFolder}/mikan-pi-chat-module-4960d27`) // 解凍したらよくわからん名前になる。
-        const newDir = new File(`${Config.modulesFolder}/ChatMi`)
-        unzippedDir.renameTo(newDir)
-        ChatLib.chat(`rename directory! 4/5`)
-        Thread.sleep(1000)
-
-        FileLib.deleteDirectory(`${Config.modulesFolder}/ChatMi.zip`)
-        ChatLib.chat(`delete zip file! "/ct load" 5/5`)
-        Thread.sleep(1000)
-        ChatLib.command("ct load", true)
-        
+        replace()
         
     } catch (e) {ChatLib.chat(`error! :${e}`)}
 })
 
-register("command", () => {
-}).setName("mi-test-copy");
 
 function whatfileName() {
     let folder = new File(`${Config.modulesFolder}`);
@@ -145,60 +112,39 @@ function readfile(module, file) {
     return FileLib.read(module, file)
 }
 
-register("command", () => {
+function replace() {
     let gitMi = Config.modulesFolder + "/" + whatfileName();
     let ChatMi = Config.modulesFolder + "/ChatMi";
     
-    // console.log("Reading ChatMi folder...");
-    let cmi = readfolder(ChatMi);  // Get file list in ChatMi folder
-    // console.log("Reading gmi folder...");
-    let gmi = readfolder(gitMi);   // Get file list in gmi folder
+    let cmi = readfolder(ChatMi);
+    let gmi = readfolder(gitMi);
 
     if (cmi && gmi) {
-        // console.log("Parsing file lists...");
-        let cmiFiles = JSON.parse(cmi);  // Convert ChatMi file names to an array
-        let gmiFiles = JSON.parse(gmi);  // Convert gmi file names to an array
-        // console.log("Parsing completed");
+        let cmiFiles = JSON.parse(cmi);
+        let gmiFiles = JSON.parse(gmi);
 
-        // Process files that exist in both cmi and gmi folders
-        for (let i = 0; i < gmiFiles.length; i++) {
-            let file = gmiFiles[i];
-            // console.log(`Processing: ${file} (${i + 1} / ${gmiFiles.length})`);
+        for (let count = 0; count < gmiFiles.length; count++) {
+            let file = gmiFiles[count];
+
             if (cmiFiles.includes(file)) {
-                // Get content of gmi file
                 let gmiContent = readfile(whatfileName(), file);
+
                 if (gmiContent) {
-                    // console.log(`Content of ${file} retrieved, starting overwrite`);
-                    // Overwrite file in ChatMi with gmi content
-                    // replacefile("ChatMi", file, gmiContent);
                     ChatLib.chat(`[cmi] ${file} has been updated.`);
-                } else {
-                    // console.log(`[Error] ${file} is empty or cannot be read.`);
-                    // ChatLib.chat(`[Error] ${file} is empty or cannot be read.`);
-                }
-            } else {
-                // console.log(`${file} does not exist in cmi folder`);
+                    
+                    // ダウンロードしたファイルを消去
+                    deletefile()
+                } 
             }
         }
 
-        // console.log("Processing completed");
     } else {
         console.log("[cmi/gmi] Error: Failed to read folder.");
         ChatLib.chat("[cmi/gmi] Error: Failed to read folder.");
     }
-}).setName("mi-test-6");
+}
 
 let item = {}
-
-register("command", () => {
-    hash("mikan-pi", "chat-module", "main", (sha) => {
-        if (sha) {  // sha をチェックする
-            ChatLib.chat(`Commit SHA: ${sha}`);
-        } else {
-            ChatLib.chat("SHAの取得に失敗しました");
-        }
-    });
-}).setName("mi-test-3");
 
 function hash(user, name, branch1, callback) {
     let url = `https://api.github.com/repos/${user}/${name}/commits/${branch1}`;
@@ -217,7 +163,6 @@ function hash(user, name, branch1, callback) {
             const jsonres = JSON.parse(response)
             console.log(jsonres)
             let commitSha = jsonres.sha;  // SHAを取得
-            Imnew = commitSha
             if (commitSha) {
                 ChatLib.chat(`&b取得したSHA: ${commitSha} 3/4`);
                 item.sha = commitSha
